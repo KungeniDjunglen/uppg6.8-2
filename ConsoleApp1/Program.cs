641feafaa93042080ea7d546fed0a3c1fc94d4f5
Admin <Admin@TEB123003>
2023-02-15 11:32:53 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static bool PrimtalsUndersökarn(int tal)
+    {
+        for (int i = tal - 1; 1 < i; i--)
+        {
+            if (tal % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static int AllaPrimtal(int tal)
+    {
+        int PrimtalNummer = 0;
+        for (; tal > 0; tal--)
+        {
+            if (PrimtalsUndersökarn(tal))
+            {
+                PrimtalNummer++;
+            }
+        }
+        return PrimtalNummer;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Skriv ett tal så undersöker hur många primtal det finns som är <= det");
+        int tal = int.Parse(Console.ReadLine());
+        Console.WriteLine("Det finns " + AllaPrimtal(tal) + " primtal mellan 0-" + tal + ".");
+
+    }
+}
